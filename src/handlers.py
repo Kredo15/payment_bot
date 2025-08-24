03c00adb7e6c1756ca5bcc808a2401725706de72
Kredo15 <kredo1511@yandex.ru>
2025-08-24 18:30:47 +0300

    add handler 'info'

--- src/handlers.py
+++ src/handlers.py
@@ -1,7 +1,9 @@
+from aiogram import F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from src.app import dp
+from src.core.message import AnswerMessage
 from src.keyboards import get_main_bk
 
 
@@ -10,4 +12,11 @@ async def start_command(message: Message):
     await message.answer(
         text='Выбери действие',
         reply_markup=get_main_bk()
+    )
+
+
+@dp.callback_query(F.data == "info")
+async def choose_category(callback: CallbackQuery):
+    await callback.message.answer(
+        text=AnswerMessage.info
     )
